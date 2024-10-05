332e6c37fa0383f5310fca581dbd21841d5a276e
osaki <motoki.osaki@hacobu.jp>
2024-10-05 22:52:12 +0900

    refactor

--- lib/app_text_theme_extension.dart
+++ lib/app_text_theme_extension.dart
@@ -1,82 +1,82 @@
 import 'package:flutter/material.dart';
 
 class AppTextThemeExtension extends ThemeExtension<AppTextThemeExtension> {
-  final TextStyle titleBold24;
-  final TextStyle titleRegular20;
-  final TextStyle titleMedium16;
+  final TextStyle titleBold;
+  final TextStyle titleMedium;
   final TextStyle titleRegular16;
+  final TextStyle titleRegular20;
+  final TextStyle headlineBold;
+  final TextStyle headlineMedium;
+  final TextStyle bodyBold14;
+  final TextStyle bodyBold16;
+  final TextStyle bodyMedium;
+  final TextStyle bodyRegular10;
   final TextStyle bodyRegular12;
   final TextStyle bodyRegular14;
-  final TextStyle headlineBold20;
-  final TextStyle headlineMedium20;
-  final TextStyle bodyBold16;
-  final TextStyle bodyMedium16;
   final TextStyle bodyRegular16;
-  final TextStyle bodyBold14;
-  final TextStyle bodyRegular10;
-  final TextStyle labelMedium16;
-  final TextStyle labelMedium14;
-  final TextStyle labelBold12;
+  final TextStyle labelBold;
   final TextStyle labelMedium12;
+  final TextStyle labelMedium14;
+  final TextStyle labelMedium16;
 
   const AppTextThemeExtension({
-    required this.titleBold24,
-    required this.titleRegular20,
-    required this.titleMedium16,
+    required this.titleBold,
+    required this.titleMedium,
     required this.titleRegular16,
+    required this.titleRegular20,
+    required this.headlineBold,
+    required this.headlineMedium,
+    required this.bodyBold14,
+    required this.bodyBold16,
+    required this.bodyMedium,
+    required this.bodyRegular10,
     required this.bodyRegular12,
     required this.bodyRegular14,
-    required this.headlineBold20,
-    required this.headlineMedium20,
-    required this.bodyBold16,
-    required this.bodyMedium16,
     required this.bodyRegular16,
-    required this.bodyBold14,
-    required this.bodyRegular10,
-    required this.labelMedium16,
-    required this.labelMedium14,
-    required this.labelBold12,
+    required this.labelBold,
     required this.labelMedium12,
+    required this.labelMedium14,
+    required this.labelMedium16,
   });
 
   @override
   AppTextThemeExtension copyWith({
-    TextStyle? titleBold24,
-    TextStyle? titleRegular20,
-    TextStyle? titleMedium16,
+    TextStyle? titleBold,
+    TextStyle? titleMedium,
     TextStyle? titleRegular16,
+    TextStyle? titleRegular20,
+    TextStyle? headlineBold,
+    TextStyle? headlineMedium,
+    TextStyle? bodyBold14,
+    TextStyle? bodyBold16,
+    TextStyle? bodyMedium,
+    TextStyle? bodyRegular10,
     TextStyle? bodyRegular12,
     TextStyle? bodyRegular14,
-    TextStyle? headlineBold20,
-    TextStyle? headlineMedium20,
-    TextStyle? bodyBold16,
-    TextStyle? bodyMedium16,
     TextStyle? bodyRegular16,
-    TextStyle? bodyBold14,
-    TextStyle? bodyRegular10,
-    TextStyle? labelMedium16,
-    TextStyle? labelMedium14,
-    TextStyle? labelBold12,
+    TextStyle? labelBold,
     TextStyle? labelMedium12,
+    TextStyle? labelMedium14,
+    TextStyle? labelMedium16,
   }) {
     return AppTextThemeExtension(
-      titleBold24: titleBold24 ?? this.titleBold24,
-      titleRegular20: titleRegular20 ?? this.titleRegular20,
-      titleMedium16: titleMedium16 ?? this.titleMedium16,
+      titleBold: titleBold ?? this.titleBold,
+      titleMedium: titleMedium ?? this.titleMedium,
       titleRegular16: titleRegular16 ?? this.titleRegular16,
+      titleRegular20: titleRegular20 ?? this.titleRegular20,
+      headlineBold: headlineBold ?? this.headlineBold,
+      headlineMedium: headlineMedium ?? this.headlineMedium,
+      bodyBold14: bodyBold14 ?? this.bodyBold14,
+      bodyBold16: bodyBold16 ?? this.bodyBold16,
+      bodyMedium: bodyMedium ?? this.bodyMedium,
+      bodyRegular10: bodyRegular10 ?? this.bodyRegular10,
       bodyRegular12: bodyRegular12 ?? this.bodyRegular12,
       bodyRegular14: bodyRegular14 ?? this.bodyRegular14,
-      headlineBold20: headlineBold20 ?? this.headlineBold20,
-      headlineMedium20: headlineMedium20 ?? this.headlineMedium20,
-      bodyBold16: bodyBold16 ?? this.bodyBold16,
-      bodyMedium16: bodyMedium16 ?? this.bodyMedium16,
       bodyRegular16: bodyRegular16 ?? this.bodyRegular16,
-      bodyBold14: bodyBold14 ?? this.bodyBold14,
-      bodyRegular10: bodyRegular10 ?? this.bodyRegular10,
-      labelMedium16: labelMedium16 ?? this.labelMedium16,
-      labelMedium14: labelMedium14 ?? this.labelMedium14,
-      labelBold12: labelBold12 ?? this.labelBold12,
+      labelBold: labelBold ?? this.labelBold,
       labelMedium12: labelMedium12 ?? this.labelMedium12,
+      labelMedium14: labelMedium14 ?? this.labelMedium14,
+      labelMedium16: labelMedium16 ?? this.labelMedium16,
     );
   }
 
@@ -86,24 +86,55 @@ class AppTextThemeExtension extends ThemeExtension<AppTextThemeExtension> {
     if (other is! AppTextThemeExtension) return this;
 
     return AppTextThemeExtension(
-      titleBold24: TextStyle.lerp(titleBold24, other.titleBold24, t)!,
-      titleRegular20: TextStyle.lerp(titleRegular20, other.titleRegular20, t)!,
-      titleMedium16: TextStyle.lerp(titleMedium16, other.titleMedium16, t)!,
+      titleBold: TextStyle.lerp(titleBold, other.titleBold, t)!,
+      titleMedium: TextStyle.lerp(titleMedium, other.titleMedium, t)!,
       titleRegular16: TextStyle.lerp(titleRegular16, other.titleRegular16, t)!,
+      titleRegular20: TextStyle.lerp(titleRegular20, other.titleRegular20, t)!,
+      headlineBold: TextStyle.lerp(headlineBold, other.headlineBold, t)!,
+      headlineMedium: TextStyle.lerp(headlineMedium, other.headlineMedium, t)!,
+      bodyBold14: TextStyle.lerp(bodyBold14, other.bodyBold14, t)!,
+      bodyBold16: TextStyle.lerp(bodyBold16, other.bodyBold16, t)!,
+      bodyMedium: TextStyle.lerp(bodyMedium, other.bodyMedium, t)!,
+      bodyRegular10: TextStyle.lerp(bodyRegular10, other.bodyRegular10, t)!,
       bodyRegular12: TextStyle.lerp(bodyRegular12, other.bodyRegular12, t)!,
       bodyRegular14: TextStyle.lerp(bodyRegular14, other.bodyRegular14, t)!,
-      headlineBold20: TextStyle.lerp(headlineBold20, other.headlineBold20, t)!,
-      headlineMedium20:
-          TextStyle.lerp(headlineMedium20, other.headlineMedium20, t)!,
-      bodyBold16: TextStyle.lerp(bodyBold16, other.bodyBold16, t)!,
-      bodyMedium16: TextStyle.lerp(bodyMedium16, other.bodyMedium16, t)!,
       bodyRegular16: TextStyle.lerp(bodyRegular16, other.bodyRegular16, t)!,
-      bodyBold14: TextStyle.lerp(bodyBold14, other.bodyBold14, t)!,
-      bodyRegular10: TextStyle.lerp(bodyRegular10, other.bodyRegular10, t)!,
-      labelMedium16: TextStyle.lerp(labelMedium16, other.labelMedium16, t)!,
-      labelMedium14: TextStyle.lerp(labelMedium14, other.labelMedium14, t)!,
-      labelBold12: TextStyle.lerp(labelBold12, other.labelBold12, t)!,
+      labelBold: TextStyle.lerp(labelBold, other.labelBold, t)!,
       labelMedium12: TextStyle.lerp(labelMedium12, other.labelMedium12, t)!,
+      labelMedium14: TextStyle.lerp(labelMedium14, other.labelMedium14, t)!,
+      labelMedium16: TextStyle.lerp(labelMedium16, other.labelMedium16, t)!,
     );
   }
 }
+
+final appTheme = ThemeData(
+  fontFamily: 'NotoSansJP',
+  extensions: const [
+    AppTextThemeExtension(
+      // Title
+      titleBold: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+      titleMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      titleRegular16: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+      titleRegular20: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+
+      // Headline
+      headlineBold: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+      headlineMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+
+      // Body
+      bodyBold14: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+      bodyBold16: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+      bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      bodyRegular10: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+      bodyRegular12: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+      bodyRegular14: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
+      bodyRegular16: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+
+      // Label
+      labelBold: TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
+      labelMedium12: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+      labelMedium14: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+      labelMedium16: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+    ),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -28,21 +28,20 @@ class Typography extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                Text('Title Bold 24px',
-                    style: customStyles(context).titleBold24),
+                Text('Title Bold 24px', style: customStyles(context).titleBold),
                 Text('Title Regular 20px',
                     style: customStyles(context).titleRegular20),
                 Text('Title Medium 16px',
-                    style: customStyles(context).titleMedium16),
+                    style: customStyles(context).titleMedium),
                 Text('Title Regular 16px',
                     style: customStyles(context).titleRegular16),
                 Text('Headline Bold 20px',
-                    style: customStyles(context).headlineBold20),
+                    style: customStyles(context).headlineBold),
                 Text('Headline Medium 20px',
-                    style: customStyles(context).headlineMedium20),
+                    style: customStyles(context).headlineMedium),
                 Text('Body Bold 16px', style: customStyles(context).bodyBold16),
                 Text('Body Medium 16px',
-                    style: customStyles(context).bodyMedium16),
+                    style: customStyles(context).bodyMedium),
                 Text('Body Regular 16px',
                     style: customStyles(context).bodyRegular16),
                 Text('Body Bold 14px', style: customStyles(context).bodyBold14),
@@ -56,8 +55,7 @@ class Typography extends StatelessWidget {
                     style: customStyles(context).labelMedium16),
                 Text('Label Medium 14px',
                     style: customStyles(context).labelMedium14),
-                Text('Label Bold 12px',
-                    style: customStyles(context).labelBold12),
+                Text('Label Bold 12px', style: customStyles(context).labelBold),
                 Text('Label Medium 12px',
                     style: customStyles(context).labelMedium12),
               ].addColumnGap(8),
@@ -68,38 +66,6 @@ class Typography extends StatelessWidget {
     );
   }
 }
-
-final appTheme = ThemeData(
-  fontFamily: 'NotoSansJP',
-  extensions: const [
-    AppTextThemeExtension(
-      // Title
-      titleBold24: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-      titleRegular20: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-      titleMedium16: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-      titleRegular16: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-
-      // Headline
-      headlineBold20: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-      headlineMedium20: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-
-      // Body
-      bodyBold16: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-      bodyMedium16: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-      bodyRegular16: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-      bodyBold14: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-      bodyRegular14: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-      bodyRegular12: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
-      bodyRegular10: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-
-      // Label
-      labelMedium16: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-      labelMedium14: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-      labelBold12: TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
-      labelMedium12: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-    ),
-  ],
-);
 
 AppTextThemeExtension customStyles(BuildContext context) =>
     Theme.of(context).extension<AppTextThemeExtension>()!;
